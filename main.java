1e50a4b4ff421ddd5e2614ee0369b5c25c31433e
unknown <1541056416@qq.com>
2022-09-22 15:39:33 +0800

    first commit

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        SchoolSystem schoolSystem = new SchoolSystem(1, 1, 0);
+        int[][] arr = {{1,1,0},{1},{2},{3},{1}};
+        schoolSystem.print(arr);
+
+    }
+}
